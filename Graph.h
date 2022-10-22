d6b548fddaf8b083c36cc91ee273a71c85a8c79d
fygure <92011471+fygure@users.noreply.github.com>
2022-10-22 13:50:54 -0500

    added priority queue way for optimal execution

--- Graph.h
+++ Graph.h
@@ -10,6 +10,7 @@
 //==========================================//
 #include <iostream>
 #include <utility>
+#include <queue>
 #include <list>
 using namespace std;
 //==========================================//
@@ -20,7 +21,7 @@ class Graph
 private:
     // Number of vertices
     int V;
-
+    int INF;
     //Store vertex and weight pair for every edge
     list< pair<int, int> >* adj;
 
@@ -40,6 +41,7 @@ Graph::Graph(int _V)
 {
     V = _V;
     adj = new list< pair<int, int> >[V];
+    INF = 999;
 }
 //==========================================//
 void Graph::addEdge(int u, int v, int w)
@@ -48,14 +50,42 @@ void Graph::addEdge(int u, int v, int w)
     adj[v].push_back(make_pair(u, w));
 }
 //==========================================//
+void Graph::shortestPath(int start)
+{
+    // Create PQ that stores vertices
+    priority_queue< pair<int, int>, vector< pair<int, int> >, greater< pair<int, int> > > PQ;
 
+    // Distance vector
+    vector<int> distance(V, INF);
 
+    PQ.push(make_pair(0, start));
+    distance[start] = 0;
 
+    // Loop until PQ becomes empty
+    while (!PQ.empty())
+    {
+        //gets minimum distance vertex
+        int u = PQ.top().second;
+        PQ.pop();
+        //iterator for custom list
+        list< pair<int, int> >::iterator i;
+        for(i = adj[u].begin(); i!= adj[u].end(); i++)
+        {
+            // Get vertex and weight
+            int v = i->first;
+            int weight = i->second;
 
-
-
-
-
-
-
+            // Relaxation
+            if (distance[v] > distance[u] + weight)
+            {
+                distance[v] = distance[u] + weight;
+                PQ.push(make_pair(distance[v], v));
+            }
+        }
+    }
+    // Display paths
+    cout << "Vertex Distances from start" << endl;
+    for(int i=0; i<V; i++)
+        cout << i << "\t\t" << distance[i] << endl;
+}
 #endif
--- solution.cpp
+++ solution.cpp
@@ -5,17 +5,31 @@
     10/22/2022
 */
 //==========================================//
-#include <iostream>
-#include <utility>
-using namespace std;
-
-//==========================================//
-// FUNCTIONS DECLARATIONS
-
+#include "Graph.h"
 //==========================================//
 int main()
 {
+    int V = 9;
+    Graph g(V);
 
+    //initializing graph
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 7, 8);
+    g.addEdge(1, 2, 8);
+    g.addEdge(1, 7, 11);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 8, 2);
+    g.addEdge(2, 5, 4);
+    g.addEdge(3, 4, 9);
+    g.addEdge(3, 5, 14);
+    g.addEdge(4, 5, 10);
+    g.addEdge(5, 6, 2);
+    g.addEdge(6, 7, 1);
+    g.addEdge(6, 8, 6);
+    g.addEdge(7, 8, 7);
+
+    //Shortest path
+    g.shortestPath(0);
 
 
     return 0;
